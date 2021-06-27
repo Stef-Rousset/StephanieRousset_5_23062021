c39efaaf0e5e431994705ad1e9ab6bb234d3d669
stephanie rousset <strabon@club-internet.fr>
2021-06-27 11:39:01 +0200

    add index page with all teddies

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+// afficher les teddies sur index.html
+const allTeddies = document.querySelector(".all-teddies");
+
+fetch("http://localhost:3000/api/teddies")
+  .then(data => data.json())
+  .then(teddiesList => {
+    teddiesList.forEach(teddy => {
+      allTeddies.insertAdjacentHTML("beforeend", `
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${teddy.imageUrl}" class="img-fluid rounded-start" alt="ours">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${teddy.name}</h5>
+              <p class="card-text">${teddy.description}</p>
+              <p class="card-text"><small class="text-muted">Prix : ${teddy.price}€</small></p>
+            </div>
+          </div>
+        </div>
+      </div>  `)
+    })
+  })
