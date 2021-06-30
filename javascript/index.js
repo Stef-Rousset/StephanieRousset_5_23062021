3a604e19cb0e138db8f527d397c95612e800ec13
stephanie rousset <strabon@club-internet.fr>
2021-06-30 11:15:58 +0200

    modify show page and improve index page

--- javascript/index.js
+++ javascript/index.js
@@ -9,23 +9,22 @@ const getAllTeddies = () => {
       .then(teddiesList => {
         teddiesList.forEach(teddy => {
           teddiesContainer.insertAdjacentHTML("afterbegin", `
-          <a href="pages/show.html">
           <div class="card mb-3 teddy-card" data-id=${teddy._id} style="max-width: 540px;">
+          <a href="pages/show.html ">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="${teddy.imageUrl}" class="img-fluid rounded-start" alt="ours">
+              <div class="col-md-4 teddy-card__img" style="background-image: url('${teddy.imageUrl}');">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">${teddy.name}</h5>
-                  <p class="card-text">${teddy.description}</p>
+                  <p class="card-text">${teddy.description.split(' ').slice(0,10).join(' ')}...</p>
                   <p class="card-text"><small class="text-muted">Couleurs disponibles : ${teddy.colors.join(', ')}</small></p>
-                  <p class="card-text">Prix : ${teddy.price}€</p>
                 </div>
               </div>
             </div>
-          </div>
-          </a>  `)
+            </a>
+          </div> `)
+
         })
         const teddiesCards = document.querySelectorAll('.teddy-card');
         teddiesCards.forEach(teddyCard => {
--- javascript/show.js
+++ javascript/show.js
@@ -1,38 +1,25 @@
 
-//recuperer l'id du teddy cliqué via les params de l'url
-// let params = (new URL(document.location)).searchParams;
-// let teddyId = params.get('id');
-
 const getOneTeddy = (id) => {
-  // selectionner l'element où afficher le teddy sur show
-  const teddyContainer = document.querySelector(".teddy-container");
-  if (teddyContainer){
-    // recuperer puis afficher le teddy sur show
+  // selectionner les elements où afficher les infos du teddy sur show
+  const teddyShow = document.querySelector(".teddy-show");
+  const teddyShowImg = document.querySelector(".teddy-img");
+  const teddyShowTitle = document.querySelector(".teddy-text__title");
+  const teddyShowText = document.querySelector(".teddy-text__text");
+  const teddyShowPrice = document.querySelector(".teddy-text__price");
+  const teddyShowColor = document.getElementById('select-color');
+
+  if (teddyShow){
+    // recuperer puis afficher les infos du teddy sur show
       fetch(`http://localhost:3000/api/teddies/${id}`)
         .then(data => data.json())
         .then(teddy => {
-            teddyContainer.insertAdjacentHTML("afterbegin", `
-            <div class="card mb-3" style="max-width: 540px;">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="${teddy.imageUrl}" class="img-fluid rounded-start" alt="ours">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">${teddy.name}</h5>
-                    <p class="card-text">${teddy.description}</p>
-                    <label for="select-color"> Choisissez une couleur</label>
-                    <select name="color" id="select-color">
-                      <option value="${teddy.colors[0]}">${teddy.colors[0]}</option>
-                      <option value="${teddy.colors[1]}">${teddy.colors[1]}</option>
-                      <option value="${teddy.colors[2]}">${teddy.colors[2]}</option>
-                      <option value="${teddy.colors[3]}">${teddy.colors[3]}</option>
-                    </select>
-                    <p class="card-text">Prix : ${teddy.price}€</p>
-                  </div>
-                </div>
-              </div>
-            </div> `)
+            teddyShowImg.setAttribute('src', `${teddy.imageUrl}`);
+            teddyShowTitle.innerText = `${teddy.name}`;
+            teddyShowText.innerText = `${teddy.description}`;
+            teddy.colors.forEach(color => {
+              teddyShowColor.insertAdjacentHTML("afterbegin", `<option value=${color}>${color}</option>`);
+            });
+            teddyShowPrice.innerText = `Prix : ${teddy.price}€`;
         })
   }
 }
